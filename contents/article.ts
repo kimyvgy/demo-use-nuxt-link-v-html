61a969d26cd6838f1166dc837d14b249152dfcb3
Nguyen Huu Kim <kimnh@webee.asia>
2020-05-30 10:42:42 +0700

    feat: initialize component use v-html

---
+++ contents/article.ts
@@ -0,0 +1,15 @@
+const contents: string = `
+<div class="about-text text-justify" style="visibility: visible;">
+  <p class="my-3"><strong>Viblo Platform</strong> là cộng đồng chia sẻ kiến thức và học tập về công nghệ thông tin đang ngày một phát triển với hơn 40 nghìn thành viên đang hoạt động; Được quản lý và phát triển bởi Viblo Team (thuộc Công ty cổ phần Sun Asterisk). </p>
+  <p class="my-3"> Với nền móng là dịch vụ <a href="https://viblo.asia" target="_blank" rel="noopener" class="underline"><strong>Viblo</strong></a> được ra mắt ngày 03.04.2015, bạn có thể viết và chia sẻ nội dung liên quan đến các vấn đề kỹ thuật như: <strong>Development</strong> (chủ đề lập trình, infrastructure...), <strong>Design</strong> (thiết kế trong quá trình sản xuất phần mềm), <strong>QA</strong> (test, đảm bảo chất lượng dự án...), <strong>Management</strong> (quản lý dự án). </p>
+  <p class="my-3"> Từ năm 2019, Viblo mở rộng thêm những dịch vụ mới để trở thành Viblo Platform bao gồm: <a href="https://code.viblo.asia" target="_blank" rel="noopener" class="underline"><strong>Viblo Code</strong></a> (dịch vụ về luyện tập kỹ năng coding, giải thuật), <a href="https://ctf.viblo.asia" target="_blank" rel="noopener" class="underline"><strong>Viblo CTF</strong></a> (dịch vụ về luyện tập kỹ năng coding, an toàn thông tin) và <a href="https://cv.viblo.asia" target="_blank" rel="noopener" class="underline"><strong>Viblo CV</strong></a> (dịch vụ tạo các mẫu CV chuyên nghiệp dành cho lập trình viên) với mong muốn vươn mình trở thành một nền tảng mở, đóng góp cho sự phát triển của cộng đồng IT Việt Nam.</p>
+
+  <p class="my-3">
+    <a href="http://localhost:3000">Back to Home Page 1</a>
+    <a href="/#hello">Back to Home Page 2</a>
+    <a href="/">Back to Home Page 3</a>
+  </p>
+</div>
+`
+
+export default contents
--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,19 +1,45 @@
 <template>
   <v-layout>
-    <v-flex class="text-center">
+    <v-flex class="my-article">
       <img
         src="/v.png"
         alt="Vuetify.js"
         class="mb-5"
       >
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
+
+      <v-card color="#02012E">
+        <v-card-title class="headline">
+          Viblo Platform
+        </v-card-title>
+
+        <v-card-subtitle>
+          The introduction below is quoted from <a href="https://about.viblo.asia">https://about.viblo.asia</a>
+        </v-card-subtitle>
+
+        <v-card-text v-html="htmlContents" />
+      </v-card>
     </v-flex>
   </v-layout>
 </template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import htmlContents from '@/contents/article'
+
+@Component
+export default class Inspire extends Vue {
+  get htmlContents (): string {
+    return htmlContents
+  }
+}
+</script>
+
+<style lang="scss">
+  .my-article {
+    .about-text {
+      &, a {
+        color: #449fdc;
+      }
+    }
+  }
+</style>
